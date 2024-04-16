551391a4b59394af4a4338413c384d36c2455173
Kolia <kyderevyanko@gmail.com>
2024-04-16 13:22:29 +0300

    some changes

--- pkg/handler/grpc/service.go
+++ pkg/handler/grpc/service.go
@@ -3,6 +3,7 @@ package grpc_service
 import (
 	"context"
 
+	"github.com/jackc/pgx/v5"
 	db "github.com/koliader/posts-post.git/internal/db/sqlc"
 	"github.com/koliader/posts-post.git/internal/pb"
 	"google.golang.org/grpc/codes"
@@ -14,7 +15,7 @@ func (s *Server) CreatePost(ctx context.Context, req *pb.CreatePostReq) (*pb.Cre
 	arg := db.CreatePostParams{
 		Title: req.GetTitle(),
 		Body:  req.GetBody(),
-		Owner: req.GetOwnerEmail(),
+		Owner: req.GetOwner(),
 	}
 	post, err := s.store.CreatePost(ctx, arg)
 	if err != nil {
@@ -26,6 +27,38 @@ func (s *Server) CreatePost(ctx context.Context, req *pb.CreatePostReq) (*pb.Cre
 	}
 	res := pb.CreatePostRes{
 		Post: convertPost(post),
+	}
+	return &res, nil
+}
+
+// * GetPost
+func (s *Server) GetPost(ctx context.Context, req *pb.GetPostReq) (*pb.GetPostRes, error) {
+	post, err := s.store.GetPostByTitle(ctx, req.Title)
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			return nil, errorResponse(codes.NotFound, "post not found")
+		}
+		return nil, errorResponse(codes.Unimplemented, "error to get post")
+	}
+	res := pb.GetPostRes{
+		Post: convertPost(post),
+	}
+	return &res, nil
+}
+
+// * ListPosts
+func (s *Server) ListPosts(ctx context.Context, req *pb.Empty) (*pb.ListPostsRes, error) {
+	var converted []*pb.PostEntity
+
+	posts, err := s.store.ListPosts(ctx)
+	if err != nil {
+		return nil, errorResponse(codes.Unimplemented, "error to list posts")
+	}
+	for _, post := range posts {
+		converted = append(converted, convertPost(post))
+	}
+	res := pb.ListPostsRes{
+		Posts: converted,
 	}
 	return &res, nil
 }
